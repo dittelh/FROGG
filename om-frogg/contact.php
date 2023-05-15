56980eacc5ef2cb0e8d9c7df8757704ad5ea845d
dittelh <dittehansen143@gmail.com>
2023-05-15 15:26:13 +0200

    rettelse af email

--- om-frogg/contact.php
+++ om-frogg/contact.php
@@ -8,10 +8,7 @@ $email = $_POST['email'];
 $message = $_POST['message'];
 
 
-// Lige nu er mailen ikke sat op med hygieias mail, derfor står Dittes mail der i stedet. 
-// Det er noget der skal skiftes ud
-// $toEmail = "neeliya@hygieia.dk";
-$toEmail = "dittehansen143@gmail.com";
+$toEmail = "frogg.helper@gmail.com";
 
 // Subject for mail
 $subject = "Kontaktform fra FROGG.dk";
@@ -22,11 +19,9 @@ $mailMessage = "En kunde har udfyldt kontaktformen på FROGG.dk.<br><br>
             Besked: $message<br><br>";
 
 // Prepare headers for mail
-// $headers =  "From: FROGG <frogg@kontakt.dk>\r\n";
-$headers =  "From: FROGG <ditte@devditte.dk>\r\n";
+$headers =  "From: FROGG <frogg.helper@gmail.com>\r\n";
 $headers .= "Reply-To: $name <$email>\r\n";
-// $headers .= "Return-Path: FROGG <frogg@kontakt.dk>\r\n";
-$headers .= "Return-Path: Hygieia <ditte@devditte.dk>\r\n";
+$headers .= "Return-Path: Hygieia <frogg.helper@gmail.com>\r\n";
 $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
