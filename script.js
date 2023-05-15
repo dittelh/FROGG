970b2e0d38b89950eac5153e7cc3a15f4f14151c
Ditte Lykke Hansen <dittehansen143@gmail.com>
2023-05-15 21:44:15 +0200

    wut

--- script.js
+++ script.js
@@ -13,13 +13,31 @@ function openMenu() {
 function openDropdown(iconId, dropdownId) {
   var iconElement = document.getElementById(iconId)
   var dropdownElement = document.getElementById(dropdownId)
+  var span1 = iconElement.children[1]
+  var span2 = iconElement.children[2]
+  var allShowingElements = document.getElementsByClassName("show-element")
+
+  console.log(allShowingElements)
+
+  for (let i = 0; i < allShowingElements.length; i++) {
+    
+    let dropdown = allShowingElements[i];
+    if(dropdown.id === dropdownId){
+      continue
+    }
+
+    let iconSide1 = dropdown.previousElementSibling.children[1].children[1]
+    let iconSide2 = dropdown.previousElementSibling.children[1].children[2]
+    
+    dropdown.classList.replace("show-element", "hide-element")
+    iconSide1.classList.replace("arrow-side-2","arrow-side-1")
+    iconSide2.classList.replace("arrow-side-1","arrow-side-2")
+
+  }
 
   dropdownElement.classList.toggle("hide-element")
   dropdownElement.classList.toggle("show-element")
 
-  var span1 = iconElement.children[1]
-  var span2 = iconElement.children[2]
- 
   span1.classList.toggle("arrow-side-1")
   span2.classList.toggle("arrow-side-2")
 
